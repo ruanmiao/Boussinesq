440b7ab75e16ff0d351d48a3ab5df66ce674b31a
Mengyao Ruan <mengyao.ruan@tri.global>
2018-07-11 13:40:13 -0400

    Unresolved: the documentation for CalcIntegralReferenceTriangle, names of CalcIntegralJm0nN1 and CalcIntegralJm1nN2

--- multibody/boussinesq_solver/math_helper.h
+++ multibody/boussinesq_solver/math_helper.h
@@ -13,35 +13,34 @@ namespace boussinesq_solver {
 /// @param p2 Second vertex.
 /// @param p3 Third vertex.
 /// @returns The are of the triangle defined by p1, p2 and p3.
-double TriangleArea(const Vector2<double>& p1,
-                    const Vector2<double>& p2,
-                    const Vector2<double>& p3);
+double CalcTriangleArea(const Vector2<double> &p1,
+                        const Vector2<double> &p2,
+                        const Vector2<double> &p3);
 
-/// This method check the orientation of the three points p1, p2, p3. Return 1
-/// if the three points are ordered counter-clockwise. -1 if clockwise. 0 if
-/// the three are co-linear.
+/// This method checks the orientation of the three points p1, p2, p3.
+/// It returns 1 if the three points are ordered counter-clockwise. -1 if
+/// clockwise. 0 if the three are co-linear.
 /// @param p1 First vertex.
 /// @param p2 Second vertex.
 /// @param p3 Third vertex.
 /// @returns The orientation of the triangle defined by p1, p2 and p3.
-int TriangleOrientation(const Vector2<double>& p1,
-                        const Vector2<double>& p2,
-                        const Vector2<double>& p3 = Eigen::MatrixXd::Zero(
-                            2, 1));
+int CalcTriangleOrientation(const Vector2<double> &p1,
+                            const Vector2<double> &p2,
+                            const Vector2<double> &p3);
 
-/// This method the integral of Jmn = int(sin^m * cos^n) in the interval
-/// [theta_0, theta_f], where m = 0, n = -1;
+/// This method returns the integral of Jₘₙ =∫ sinᵐ x cosⁿ x dx
+/// in the interval x ∈ [θ₀, θf], where m = 0, n = -1;
 /// @param theta_0.
 /// @param theta_f.
 /// @returns the integral Jmn.
-double IntegralJm0nN1(const double& theta_0, const double& theta_f);
+double CalcIntegralJm0nN1(double theta_0, double theta_f);
 
-/// This method the integral of Jmn = int(sin^m * cos^n) in the interval
-/// [theta_0, theta_f], where m = 1, n = -2;
+/// This method returns the integral of Jₘₙ =∫ sinᵐ x cosⁿ x dx
+/// in the interval x ∈ [θ₀, θf], where m = 1, n = -2;
 /// @param theta_0.
 /// @param theta_f.
 /// @returns the integral Jmn.
-double IntegralJm1nN2(const double& theta_0, const double& theta_f);
+double CalcIntegralJm1nN2(double theta_0, double theta_f);
 
 
 }  // namespace boussinesq_solver
