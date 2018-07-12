9552127f7ea0cc93fdf4458821802bcb43670e96
Mengyao Ruan <mengyao.ruan@tri.global>
2018-07-12 11:06:47 -0400

    fixed a bug during the merge process

--- multibody/boussinesq_solver/test_helper.h
+++ multibody/boussinesq_solver/test_helper.h
@@ -10,12 +10,15 @@ namespace multibody {
 namespace boussinesq_solver {
 
 /// Given the 2D Parallelogram area defined by the 4 vertices, with the number
-/// of elements for each edge specified. The layout of the mesh looks like
-/// the following:
+/// of elements for each edge specified.
+//  TODO: The layout of the mesh.
 /// @param p1 First vertex.
 /// @param p2 Second vertex.
 /// @param p3 Third vertex.
-/// @returns The are of the triangle defined by p1, p2 and p3.
+/// @param p4 Third vertex.
+/// @returns The first vector in the return is the location of all nodes in
+/// the mesh. The second vector in the return is the node indexes of all the
+/// triangles in the mesh.
 std::pair<std::vector<Eigen::Vector3d>, std::vector<Eigen::Vector3i>>
 MeshSquare(
     const Vector2<double>& p1, const Vector2<double>& p2,
