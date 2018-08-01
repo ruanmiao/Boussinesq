2267fcaf7c78ffb01559fced7e2461c29e1e1339
amcastro-tri <alejandro.castro@tri.global>
2018-08-01 11:46:35 -0700

    Adds methods to write VTK. Implements example readin OBJ and writting vtk

---
+++ geometry/mesh_query/examples/read_obj_write_vtk.cc
@@ -0,0 +1,39 @@
+#include <memory>
+#include <vector>
+
+#include <gflags/gflags.h>
+
+#include "drake/common/find_resource.h"
+#include "drake/geometry/mesh_query/vtk_io.h"
+#include "drake/multibody/shapes/geometry.h"
+
+namespace drake {
+namespace geometry {
+namespace mesh_query {
+namespace {
+
+using ::DrakeShapes::Mesh;
+
+int DoMain() {
+
+  std::vector<Vector3<double>> points;
+  std::vector<Vector3<int>> triangles;
+
+  const auto file_name =
+      FindResourceOrThrow("drake/geometry/mesh_query/examples/sphere.obj");
+  Mesh mesh_loader(file_name, file_name);
+  mesh_loader.LoadObjFile(&points, &triangles);
+
+  OutputMeshToVTK("sphere.vtk", points, triangles);
+  return 0;
+}
+
+}  // namespace
+}  // namespace mesh_query
+}  // namespace geometry
+}  // namespace drake
+
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  return drake::geometry::mesh_query::DoMain();
+}
---
+++ geometry/mesh_query/vtk_io.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <fstream>
+#include <memory>
+#include <vector>
+
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
+#include "drake/common/drake_assert.h"
+#include "drake/common/drake_copyable.h"
+#include "drake/common/drake_throw.h"
+#include "drake/common/eigen_types.h"
+
+namespace drake {
+namespace geometry {
+namespace mesh_query {
+
+void OutputMeshToOBJ(
+    const std::string& file_name,
+    const std::vector<Vector3<double>>& points,
+    const std::vector<Vector3<int>>& triangles) {
+  const int num_nodes = points.size();
+  const int num_tris = triangles.size();
+
+  std::ofstream file(file_name);
+
+  for (int i_node = 0; i_node < num_nodes; i_node++) {
+    const Vector3<double> pos = points[i_node];
+    file << "v " << pos[0] << " " << pos[1]
+         << " " << pos[2] << std::endl;
+  }
+
+  file << std::endl;
+  for (int i_tri = 0; i_tri < num_tris; i_tri++) {
+    const Vector3<int> tri = triangles[i_tri];
+    file << "f " << tri[0] + 1 << " " << tri[1] + 1
+         << " " << tri[2] + 1 << std::endl;
+  }
+  file.close();
+}
+
+void OutputMeshToVTK(
+    const std::string& file_name,
+    const std::vector<Vector3<double>>& points,
+    const std::vector<Vector3<int>>& triangles) {
+  const int num_nodes = points.size();
+  const int num_tris = triangles.size();
+
+  std::ofstream file(file_name);
+
+  // Header for the VTK file.
+  file << "# vtk DataFile Version 3.0" << std::endl;
+  file << "Visualize mesh data" << std::endl;
+  file << "ASCII" << std::endl;
+  file << std::endl;
+  file << "DATASET UNSTRUCTURED_GRID" << std::endl;
+
+  // Header fot he points data.
+  file << "POINTS " << num_nodes << " double" << std::endl;
+
+  // Write the points data.
+  for (const auto& point : points) {
+    file << fmt::format("{:.8f} {:.8f} {:.8f}\n", point[0], point[1], point[2]);
+    //file << point[0] << " " << point[1] << " " << point[2] << std::endl;
+  }
+  file << std::endl;
+
+  // Header for the elements (triangles in this case).
+  file << "CELLS " << num_tris << " " << num_tris * 4 << std::endl;
+
+
+  for (const auto& triangle : triangles) {
+    file << "3 " << triangle[0] << " " << triangle[1] << " " << triangle[2] <<
+        std::endl;
+  }
+  file << std::endl;
+
+  // VTK needs us to spell out the element type for each element.
+  file << "CELL_TYPES " << num_tris << std::endl;
+  for (int i_tri = 0; i_tri < num_tris; i_tri++) {
+    file << "5" << std::endl;
+  }
+  file << std::endl;
+
+  file << std::endl;
+  file.close();
+}
+
+}  // namespace mesh_query
+}  // namespace geometry
+}  // namespace drae
