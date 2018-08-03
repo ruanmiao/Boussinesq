c9d9839ba930cc805e75c12feb1fe4dc31de1695
amcastro-tri <alejandro.castro@tri.global>
2018-08-03 10:50:09 -0700

    Implements CalcClosestPointOnTriangle()

--- geometry/mesh_query/mesh_query.cc
+++ geometry/mesh_query/mesh_query.cc
@@ -408,6 +408,71 @@ void FlipNormals(Mesh<double>* mesh) {
   }
 }
 
+/// This method computes the closest point P' on the triangle abc to point p.
+/// Algorithm in Section 5.1.5 of Ericson, C., 2004. Real-time collision
+/// detection. CRC Press.
+Vector3<double> CalcClosestPointOnTriangle(
+    const Vector3<double>& p,
+    const Vector3<double>& a,
+    const Vector3<double>& b,
+    const Vector3<double>& c) {
+  const Vector3<double> ab = b - a;
+  const Vector3<double> ac = c - a;
+  const Vector3<double> bc = c - b;
+
+  // Compute parametric position s for projetion P' of P on AB,
+  // P' = A + s*AB, s = snom/(snom+sdenom).
+  const double snom = ab.dot(p-a);
+  const double sdenom = (p-b).dot(a-b);
+
+  // Compute parametric position t for projection P' of P on AC,
+  // P' = A + t*AC, s = tnom/(tnom+tdenom).
+  const double tnom = ac.dot(p-a);
+  const double tdenom = (p-c).dot(a-c);
+
+  if (snom <= 0.0 && tnom <= 0.0) return a;  // Vertex region early out.
+
+  // Compute parametric position u for projection P' of P on BC,
+  // P' = B + u*BC, u = unom/(unom+udenom).
+  const double unom = bc.dot(p-b);
+  const double udenom = (p-c).dot(b-c);
+
+  if (sdenom <= 0.0 && unom <= 0.0) return b;    // Vertex region early out.
+  if (tdenom <= 0.0 && udenom <= 0.0) return c;  // Vertex region early out.
+
+  // P is outside (or on) AB if the triple product [N PA PB] <= 0.
+  const Vector3<double> n = (b-a).cross(c-a);
+  const double vc = n.dot((a-p).cross(b-p));
+
+  // If P is outside AB and within feature region of AB, return the projection
+  // of P onto AB.
+  if (vc <= 0.0 && snom >= 0.0 && sdenom >= 0.0)
+    return a + snom/(snom+sdenom) * ab;
+
+  // P is outside (or on) BC if the triple product [N PB PC] <= 0.
+  const double va = n.dot((b-p).cross(c-p));
+
+  // If P is outside BC and within feature region of BC,
+  // return projection of P onto BC.
+  if (va <= 0.0 && unom >= 0.0 && udenom >= 0.0)
+    return b + unom/(unom+udenom)*bc;
+
+  // P is outside (or on) CA if the triple product [N PC PA] <= 0.
+  const double vb = n.dot((c-p).cross(a-p));
+
+  // If P is outside CA and within feature region of CA, return the projection
+  // of P onto CA.
+  if (vb <= 0 && tnom >= 0.0 && tdenom >= 0.0)
+    return a + tnom/(tnom+tdenom)*ac;
+
+  // P must project inside the face region. Compute Q using barycentric
+  // coordinates.
+  const double u = va/(va+vb+vc);
+  const double v = vb/(va+vb+vc);
+  const double w = 1.0 - u - v;  // = vc//(va+vb+vc).
+  return u * a + v * b + w * c;
+}
+
 }  // namespace mesh_query
 }  // namespace geometry
 }  // namespace drake
